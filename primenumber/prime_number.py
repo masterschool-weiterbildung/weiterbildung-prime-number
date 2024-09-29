435aeb9f628daabedb7afaa96040bebe558eff0d
Jerome de Dios <182743031+jdedios-de@users.noreply.github.com>
2024-09-29 14:28:34 +0200

    Prime Number: Submitted to School

--- primenumber/prime_number.py
+++ primenumber/prime_number.py
@@ -1,85 +1,105 @@
 NUMBER_BEGIN_TO_CHECK = 2
 
-def is_number_divisible_by_itself_and_greater_than_one(number_to_check):
+def is_divisible_by(number: int, by: int) -> bool:
     """
-    Check if a given number is greater than one and divisible by itself.
-
-    A number is considered valid if it meets the following criteria:
-    - It is greater than one.
-    - It is an integer (divisible by 1).
-    - It is divisible by itself.
+    Check if a number is divisible by another number.
 
     Parameter:
-        number_to_check (int or float): The number to check for the conditions.
+        number (int): The number to be divided.
+        by (int): The number by which to divide.
 
     Returns:
-        bool: True if the number is greater than one and divisible by itself,
-              False otherwise.
+        bool: True if `number` is divisible by `by`, False otherwise.
+    """
+    return number % by == 0
+
+
+def is_prime(number: int) -> bool:
+    """
+    Determine if a number is prime.
+
+    Parameter:
+        number (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
+    return not is_number_divisible_by_other_number(number, number)
+
+
+def is_number_divisible_by_itself_and_greater_than_one(number_to_check: int) -> bool:
+    """
+    Check if a number is greater than one and divisible by itself and 1.
+
+    Parameter:
+        number_to_check (int): The number to check.
+
+    Returns:
+        bool: True if the number is greater than 1 and divisible by both itself and 1.
     """
     return True if (number_to_check > 1
-                    and number_to_check % 1 == 0
-                    and number_to_check % number_to_check == 0) \
-                else False
+                    and is_divisible_by(number_to_check, 1)
+                    and is_divisible_by(number_to_check, number_to_check)) \
+        else False
 
-def is_number_divisible_by_other_number(number_to_check, end_range):
+
+def is_number_divisible_by_other_number(number_to_check: int, end_range: int) -> bool:
     """
-    Check if a given number is divisible by any number in a specified range.
+    Check if a number is divisible by any number within a given range.
 
-    This function evaluates whether `number_to_check` is divisible by any integer
-    within the range starting from `NUMBER_BEGIN_TO_CHECK` to `end_range` (exclusive).
-    It also checks if `number_to_check` is greater than one and divisible by itself.
-
-    Args:
-        number_to_check (int): The number to check for divisibility.
-        end_range (int): The exclusive upper limit of the range to check against.
+    Parameter:
+        number_to_check (int): The number to check.
+        end_range (int): The end of the range to check divisibility.
 
     Returns:
-        bool: True if `number_to_check` is divisible by any number in the range
-              and meets the self-divisibility condition, False otherwise.
-
-    Note:
-        NUMBER_BEGIN_TO_CHECK is 2 declared globally.
+        bool: True if `number_to_check` is divisible by any number in the range,
+              False otherwise.
     """
     return any(num is None or num > 0
                for num in range(NUMBER_BEGIN_TO_CHECK, end_range)
-               if (number_to_check % num == 0
+               if (is_divisible_by(number_to_check, num)
                    and is_number_divisible_by_itself_and_greater_than_one(
-            number_to_check)))
+                number_to_check)))
 
-def is_prime_number(number_to_check, end_range):
+
+def primes_in_range(start_range: int, end_range: int) -> None:
     """
-    Determine if a given number is prime.
-
-    A prime number is defined as a natural number greater than 1 that cannot
-    be formed by multiplying two smaller natural numbers. This function checks
-    whether `number_to_check` is not divisible by any number in the range from
-    `NUMBER_BEGIN_TO_CHECK` to `end_range`.
+    Print prime numbers in a given range.
 
     Parameter:
-        number_to_check (int): The number to check for primality.
-        end_range (int): The exclusive upper limit of the range to check against.
+        start_range (int): The starting number of the range.
+        end_range (int): The ending number of the range.
 
     Returns:
-        bool: True if `number_to_check` is prime (not divisible by any number
-              in the specified range), False otherwise.
-
-    Note:
-        NUMBER_BEGIN_TO_CHECK is 2 declared globally.
+        None
     """
-    return not is_number_divisible_by_other_number(number_to_check, end_range)
-
-def input_start_and_end_number():
-    return int(input("Enter start range: ")), int(input("Enter end range: "))
-
-def display_the_prime_numbers(start_range, end_range):
     for num_to_check in range(start_range, end_range):
-        if is_prime_number(num_to_check, start_range):
+        if is_prime(num_to_check):
             print(f"The number {num_to_check} is prime")
 
-def main():
+
+def input_start_and_end_number() -> tuple:
+    """
+    Prompt the user to input the start and end of the range.
+
+    Returns:
+        tuple: A tuple containing the start and end numbers as integers.
+    """
+    return int(input("Enter start range: ")), int(input("Enter end range: "))
+
+
+def main() -> None:
+    """
+    Main function that gets user input and prints prime numbers in the specified range.
+
+    Returns:
+        None
+    """
     start_range, end_range = input_start_and_end_number()
 
-    display_the_prime_numbers(start_range, end_range)
+    primes_in_range(start_range, end_range)
+
 
 if __name__ == "__main__":
+
     main()
--- tests/test_prime_number_pytest.py
+++ tests/test_prime_number_pytest.py
@@ -14,31 +14,31 @@ class TestPrimeNumber:
     def test_is_number_divisible_by_itself_and_greater_than_one(self, number_to_check):
         assert prime_number.is_number_divisible_by_itself_and_greater_than_one(number_to_check) == True
 
-    @pytest.mark.parametrize("number_to_check_if_prime, end_range",
+    @pytest.mark.parametrize("number_to_check_if_prime",
                              [
-                                 (11, 10),
-                                 (13, 10),
-                                 (17, 10),
-                                 (19, 10)
+                                 (11),
+                                 (13),
+                                 (17),
+                                 (19)
                              ]
                              )
 
-    def test_is_prime_number(self, number_to_check_if_prime, end_range):
-        assert prime_number.is_prime_number(number_to_check_if_prime, end_range) == True
+    def test_is_primer(self, number_to_check_if_prime):
+        assert prime_number.is_prime(number_to_check_if_prime) == True
 
-    @pytest.mark.parametrize("number_to_check_if_prime, end_range",
+    @pytest.mark.parametrize("number_to_check_if_prime",
                              [
-                                 (10, 10),
-                                 (12, 10),
-                                 (14, 10),
-                                 (15, 10),
-                                 (16, 10),
-                                 (18, 10)
+                                 (10),
+                                 (12),
+                                 (14),
+                                 (15),
+                                 (16),
+                                 (18)
                              ]
                              )
 
-    def test_is_not_prime_number(self, number_to_check_if_prime, end_range):
-        assert prime_number.is_prime_number(number_to_check_if_prime, end_range) == False
+    def test_is_not_prime(self, number_to_check_if_prime):
+        assert prime_number.is_prime(number_to_check_if_prime) == False
 
     @pytest.mark.parametrize("number_to_check_if_prime, end_range",
                              [
